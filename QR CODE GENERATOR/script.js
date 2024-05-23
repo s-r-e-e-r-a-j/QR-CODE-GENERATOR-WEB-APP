100a98c6d1393bf13ecf842842f9a08a4eec57fc
C0DEGamer <154326486+C0DEGamer@users.noreply.github.com>
2024-05-23 19:30:13 +0530

    Add files via upload

---
+++ QR CODE GENERATOR/script.js
@@ -0,0 +1,29 @@
+
+		
+const a=document.querySelector(".D-Link");
+const image=document.querySelector(".image");
+const generatebtn=document.querySelector(".G-btn");
+const input=document.querySelector(".input");
+
+generatebtn.addEventListener("click",()=>{
+	image.style.display="block"
+let content=input.value;
+image.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${content}`
+//click the qr image to download
+ 
+//download section   //coded by sreeraj
+var api=` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${content}`;
+
+fetch(api).then((response)=>response.blob()).then((data)=>{
+	console.log(data)
+  let url=URL.createObjectURL(data);
+  a.href=url;
+  a.download="QRIMAGE";
+ 
+})
+
+})
+
+
+
+	
